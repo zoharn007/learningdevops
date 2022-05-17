4ac0f507d3c750d6a54100c8c12a81c19785bf7e
NEWCO\znagar <zohar.nagar@nex.com>
2022-05-17 12:51:33 +0300

    def fibonacci_fixme(n):

--- python_katas/kata_2/sketches/fibonacci.py
+++ python_katas/kata_2/sketches/fibonacci.py
@@ -1,73 +1,28 @@
-n = int(input("What Fibonacci number would you like to find? "))
+def fibonacci_fixme(n):
+    """
+    2 Kata
+    A Fibonacci sequence is the integer sequence of 1, 1, 2, 3, 5, 8, 13....
+    The first two terms are 1 and 1. All other terms are obtained by adding the preceding two terms.
+    This function should return the n'th element of fibonacci sequence. As following:
+    fibonacci_fixme(1) -> 1
+    fibonacci_fixme(2) -> 1
+    fibonacci_fixme(3) -> 2
+    fibonacci_fixme(4) -> 3
+    fibonacci_fixme(5) -> 5
+    But it doesn't (it has some bad lines in it...)
+    You should (1) correct the for statement and (2) swap two lines, so that the correct fibonacci element will be returned
+    """
+    a = 0
+    b = 1
+    for i in range(1, n):
+        tmp = a + b
+        a = b
+        b = tmp
 
-def fib(n):
-    i = 0
-    nextterm = 1
-    present = 1
-    previous = 0
-
-    while i < n:
-        nextterm = present + previous
-        present = previous
-        previous = nextterm
-        i = i + 1
-        #print nextterm
-
-    return nextterm
-
-print(fib(n))
-
+    return b
 
 
 if __name__ == '__main__':
-    print('\nvalid_parentheses:\n--------------------')
-    print(valid_parentheses('[[{()}](){}]'))
 
     print('\nfibonacci_fixme:\n--------------------')
     print(fibonacci_fixme(6))
-
-    print('\nmost_frequent_name:\n--------------------')
-    print(most_frequent_name('names.txt'))
-
-    print('\nfiles_backup:\n--------------------')
-    print(files_backup('python_katas/kata_2'))
-
-    print('\nreplace_in_file:\n--------------------')
-    print(replace_in_file('mnist-predictor.yaml', '{{IMG_NAME}}', 'mnist-pred:0.0.1'))
-
-    print('\njson_configs_merge:\n--------------------')
-    print(json_configs_merge('default.json', 'local.json'))
-
-    print('\nmonotonic_array:\n--------------------')
-    print(monotonic_array([1, 2, 3, 6, 8, 9, 0]))
-
-    print('\nmatrix_avg:\n--------------------')
-    print(matrix_avg([[1, 2, 3], [4, 5, 6], [7, 8, 9]], rows=[0, 2]))
-    print(matrix_avg([[1, 2, 3], [4, 5, 6], [7, 8, 9]]))
-
-    print('\nmerge_sorted_lists:\n--------------------')
-    print(merge_sorted_lists([1, 4, 77, 9, 13343], [-7, 0, 7, 23]))
-
-    print('\nlongest_common_substring:\n--------------------')
-    print(longest_common_substring('abcdefg', 'bgtcdesd'))
-
-    print('\nlongest_common_prefix:\n--------------------')
-    print(longest_common_prefix('abcd', 'ttty'))
-
-    print('\nrotate_matrix:\n--------------------')
-    print(rotate_matrix([[1, 2, 3, 4, 5], [6, 7, 8, 9, 10]]))
-
-    print('\nis_valid_email:\n--------------------')
-    print(is_valid_email('israel.israeli@gmail.com'))
-
-    print('\npascal_triangle:\n--------------------')
-    print(pascal_triangle(4))
-
-    print('\nlist_flatten:\n--------------------')
-    print(list_flatten([1, 2, [3, 4, [4, 5], 7], 8]))
-
-    print('\nstr_compression:\n--------------------')
-    print(str_compression('aaaabdddddhgf'))
-
-    print('\nstrong_pass:\n--------------------')
-    print(strong_pass('##$FgC7^^5a'))
