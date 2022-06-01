04f1ea94e7e3f3241413222a01576ede9e01aa4a
Ariel Karas <ariel1434@gmail.com>
2022-06-01 11:48:49 +0300

    is_valid_emil.py haven't beeing saved after we worked on it

--- python_katas/kata_2/sketches/is_valid_email.py
+++ python_katas/kata_2/sketches/is_valid_email.py
@@ -1,47 +1,49 @@
 def is_valid_email(mail_str):
     """
-    3 Kata
+        3 Kata
 
-    This function returns True if the given mail is in the form:
-    (username)@(domainname)
+        This function returns True if the given mail is in the form:
+        (username)@(domainname)
 
-    Where
-    * (username) must start with digit or an English character, and can contains only 0-9 a-z A-Z . or _
-    * (domainname) is a real, existed domain - one that resolves to an actual ip address
+        Where
+        * (username) must start with digit or an English character, and can contains only 0-9 a-z A-Z . or _
+        * (domainname) is a real, existed domain - one that resolves to an actual ip address
 
-    Hint: use socket.gethostbyname() to resolve a DNS in Python code
+        Hint: use socket.gethostbyname() to resolve a DNS in Python code
 
-    :param mail_str: mail to check
-    :return: bool: True if it's a valid mail (otherwise either False is returned or the program can crash)
-    """
+        :param mail_str: mail to check
+        :return: bool: True if it's a valid mail (otherwise either False is returned or the program can crash)
+        """
 
-import socket
+    import socket
+    mail_check = mail_str.split("@", 1)
+    username = mail_check[0]
+    hostName = mail_check[1]
 
-x = '1@yneaaaaaat.co.il'
-w = x.split("@", 1)
-first = w[0]
-hostName = w[1]
-flag_1 = True
+    def username_check(username):
+        for i in username[::]:
+            if not i.isalpha() and not i.isnumeric():
+                return False
+            else:
+                pass
+        return True
 
-for i in first[::]:
-    if flag_1 == False:
-        break
-    if i.isalpha() or i.isnumeric():
-        flag_1 = True
+    def hostName_check(hostName):
+        try:
+            ipAddress = socket.gethostbyname(hostName)
+            #w = ("IP address of the host name {} is: {}".format(hostName, ipAddress)
+        except Exception as Eror:
+            return False
+        return True
+
+    run = "hostName_check(hostName)"
+    if username_check(username) and hostName_check(hostName):
+        return True
     else:
-        flag_1 = False
-
-if flag_1:
-    try:
-        ipAddress = socket.gethostbyname(hostName)
-        print("IP address of the host name {} is: {}".format(hostName, ipAddress))
-    except Exception as e:
-        print(e)
-else:
-    print("False")
-
+        return False
 
 
 if __name__ == '__main__':
     print('\nis_valid_email:\n--------------------')
     print(is_valid_email('israel.israeli@gmail.com'))
+
